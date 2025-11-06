71d0692ed7ec694940170a5738d5b5c41c4e7978
SauloCunha02 <saulocunha01@gmail.com>
2025-11-06 12:53:58 -0300

    rotas: inkection, get e post, any

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,4 +8,8 @@ Route::get('/', function () {
 });
 Route::get('/user', function(){
     return '<h1> Aqui esta o ursuário</h1>';
+});
+
+Route::get('/injetion', function(Request $request){
+    return var_dump($request);
 });
