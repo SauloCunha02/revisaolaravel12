95e3038419d08d525225cbc150e4e6471981b88a
SauloCunha02 <saulocunha01@gmail.com>
2025-11-06 12:50:04 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/user', function(){
+    return '<h1> Aqui esta o ursuário</h1>';
+});
