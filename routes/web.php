390c4fb6de4f56de92bc9cc124830e859073301a
SauloCunha02 <saulocunha01@gmail.com>
2025-11-06 15:28:21 -0300

    routes laravel 12

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,14 @@ Route::get('/user', function(){
 Route::get('/injetion', function(Request $request){
     return var_dump($request);
 });
+Route::match(['get', 'post'], function(Request $request){
+    return var_dump($request);
+    //aceita get e post
+});
+Route::any('/any', function(Request $request){
+     return var_dump($request);
+     //aceita qualquer HTTP reverb
+});
+Route::redirect('/saltar', '/'); // redireciona para a rota raiz status:302
+Route::permanentRedirect('/saltar2', '/'); // redireciona para rota raiz status:301
+ROute::view('/view', 'home'); // redirecionamento para uma view
