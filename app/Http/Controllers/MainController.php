d4689f6dfdbacafb94b4b77763823acae3956d42
SauloCunha02 <saulocunha01@gmail.com>
2025-11-07 10:13:06 -0300

    passando ados da controller para view 2

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,10 +8,18 @@ use Illuminate\Contracts\View\View;
 class MainController extends Controller
 {
     public function showView():View{
+        //método 1
+        /*
          $data = [
             'name' => "Saulo",
             'phone' => "8822331122"
          ];
         return view('admin.newPage', $data);
+        */
+        return view('admin.newPage')
+        ->with('name', "Saulo")
+        ->with('phone', "8822331122");
+
+
     }
 }
