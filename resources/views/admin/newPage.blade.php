0ca6ed8202594a7316217af98123b3e156d07a63
SauloCunha02 <saulocunha01@gmail.com>
2025-11-07 12:51:57 -0300

    empty

--- resources/views/admin/newPage.blade.php
+++ resources/views/admin/newPage.blade.php
@@ -1,16 +1,9 @@
 <div>
     {{-- instrução if simples --}}
-@switch($value)
-    @case(100)
-        <h1>é igual a 100 </h1>    
-        @break
-     @case(200)
-        <h1>é igual a 200 </h1>    
-        @break
-     @case(300)
-        <h1>é igual a 300 </h1>    
-        @break
-     @default
-        <h1>Outro<h1>    
-@endswitch
+    {{--Verifica se a variavel está vazia ou nula--}}
+@empty($value)
+    <h1>Vazio</h1>
+@else
+    <h1>Existe</h1>
+@endempty
 </div>
